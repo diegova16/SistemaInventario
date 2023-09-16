c73f4946eb69dc7b21d010dc9bcf950ba1d8bb59
diegova16 <dvargas@grupoasesor.net>
2023-09-16 17:27:11 -0600

    Repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> _dbSet;
+
+        public Repositorio(ApplicationDbContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<T>();
+        }
+
+        public async Task Agregar(T entidad)
+        {
+            await _dbSet.AddAsync(entidad);
+        }
+
+        public async Task<T> Obtener(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filtro != null) {
+                query = query.Where(filtro);
+            }
+
+            if (incluirPropiedades != null) {
+                foreach (var propiedad in incluirPropiedades.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries ))
+                {
+                    query = query.Include(propiedad);
+                }
+            }
+
+            if (orderBy != null) {
+                query = orderBy(query);
+            }
+
+            if (!isTracking) {
+                query = query.AsNoTracking();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+
+            if (incluirPropiedades != null)
+            {
+                foreach (var propiedad in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(propiedad);
+                }
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public void Remover(T entidad)
+        {
+            _dbSet.Remove(entidad);
+        }
+
+        public void RemoverRango(IEnumerable<T> entidad)
+        {
+            _dbSet.RemoveRange(entidad);
+        }
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorio/UnidadTrabajo.cs
@@ -0,0 +1,32 @@
+﻿using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class UnidadTrabajo : IUnidadTrabajo
+    {
+        private readonly ApplicationDbContext _bd;
+        public IBodegaRepositorio bodega { get; private set; }
+
+        public UnidadTrabajo(ApplicationDbContext bd)
+        {
+            _bd = bd;
+            bodega = new BodegaRepositorio(_bd);
+        }
+
+        public void Dispose()
+        {
+            _bd.Dispose();
+        }
+
+        public async Task Guardar()
+        {
+            await _bd.SaveChangesAsync();
+        }
+    }
+}
