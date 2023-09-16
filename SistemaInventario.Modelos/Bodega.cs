48a398c1cebc7621d8d55d30c14c7dc4d4d0f42a
diegova16 <dvargas@grupoasesor.net>
2023-09-16 15:30:52 -0600

    Configuración del proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "La longitud máxima de Nombre es 60 caracteres")]
+        public String Nombre { get; set; }
+
+
+        [Required(ErrorMessage = "Descripción es requerido")]
+        [MaxLength(60, ErrorMessage = "La longitud máxima de Descripción es 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
